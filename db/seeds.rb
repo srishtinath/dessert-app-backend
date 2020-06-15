fb14bcec9cf862dc26f52cac06392edc00ad7402
Sofia Tofigh <sofiatofigh0@gmail.com>
2020-06-15 18:23:13 -0400

    more pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,11 @@ Ingredient.delete_all
 RecipeIngredient.delete_all
 
 chocolate_chip_cookies = Recipe.create(name: 'Chocolate Chip Cookies', difficulty_level: 1)
+brownies = Recipe.create(name: 'Brownies', difficulty_level: 3)
+lemon_bars = Recipe.create(name: 'Lemon Bars', difficulty_level: 7)
+molten_chocolate_lava_cake = Recipe.create(name: 'Molten Chocolate Lava Cake', difficulty_level: 6)
+banana_bread = Recipe.create(name: 'Banana Bread', difficulty_level: 2)
+cinnammon_roll = Recipe.create(name: 'Cinnammon Rolls', difficulty_level: 5)
 
 
 butter = Ingredient.create(name: 'Butter', image: 'butter.png')
@@ -19,6 +24,9 @@ salt = Ingredient.create(name: 'Salt', image: 'salt.png')
 flour = Ingredient.create(name: 'Flour', image: 'flour.png')
 baking_soda = Ingredient.create(name: 'Baking Soda', image: 'baking soda.png')
 chocolate_chip = Ingredient.create(name: 'Chocolate Chip', image: 'chocolate chip.png')
+cocoa_powder = Ingredient.create(name: 'Cocoa Powder', image: 'cocoa.png')
+nuts = Ingredient.create(name: 'Nuts', image: 'nuts.png')
+
 
 RecipeIngredient.create(ingredient_id: butter.id, recipe_id: chocolate_chip_cookies.id)
 RecipeIngredient.create(ingredient_id: sugar.id, recipe_id: chocolate_chip_cookies.id)
@@ -28,5 +36,14 @@ RecipeIngredient.create(ingredient_id: flour.id, recipe_id: chocolate_chip_cooki
 RecipeIngredient.create(ingredient_id: baking_soda.id, recipe_id: chocolate_chip_cookies.id)
 RecipeIngredient.create(ingredient_id: chocolate_chip.id, recipe_id: chocolate_chip_cookies.id)
 
+RecipeIngredient.create(ingredient_id: butter.id, recipe_id: brownies.id)
+RecipeIngredient.create(ingredient_id: sugar.id, recipe_id: brownies.id)
+RecipeIngredient.create(ingredient_id: cocoa_powder.id, recipe_id: brownies.id)
+RecipeIngredient.create(ingredient_id: vanilla.id, recipe_id: brownies.id)
+RecipeIngredient.create(ingredient_id: eggs.id, recipe_id: brownies.id)
+RecipeIngredient.create(ingredient_id: flour.id, recipe_id: brownies.id)
+RecipeIngredient.create(ingredient_id: baking_soda.id, recipe_id: brownies.id)
+RecipeIngredient.create(ingredient_id: salt.id, recipe_id: brownies.id)
+RecipeIngredient.create(ingredient_id: nuts.id, recipe_id: brownies.id)
 
 
